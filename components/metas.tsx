632a3a73e7ee6bc768de8767e5ed1e3c77fc8a53
Lucas Consejo <lucas.consejo@ynov.com>
2022-03-14 14:42:15 +0100

    ✨ feat: add metas component
    Add metas component for metas tags

---
+++ components/metas.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+
+type MetasProps = {
+    title: string,
+    description?: string
+}
+
+
+export default function Metas({ title, description }: MetasProps) {
+    return (
+        <Head>
+            <title>{title}</title>
+            <meta name="description" content={description ?? description } />
+            <link rel="icon" href="/favicon.ico" />
+            <meta name="theme-color" content="rgb(26, 8, 66)"></meta>
+        </Head>
+    );
+}
