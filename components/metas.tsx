3684c5f0583c35926095c81c25033094fc1381d4
Lucas Consejo <lucas.consejo@ynov.com>
2022-03-14 14:44:47 +0100

    ♻️ refactor: update metas component with prettier
    Update metas component file with prettier

--- components/metas.tsx
+++ components/metas.tsx
@@ -1,18 +1,17 @@
-import Head from 'next/head';
+import Head from "next/head";
 
 type MetasProps = {
-    title: string,
-    description?: string
-}
-
+  title: string;
+  description?: string;
+};
 
 export default function Metas({ title, description }: MetasProps) {
-    return (
-        <Head>
-            <title>{title}</title>
-            <meta name="description" content={description ?? description } />
-            <link rel="icon" href="/favicon.ico" />
-            <meta name="theme-color" content="rgb(26, 8, 66)"></meta>
-        </Head>
-    );
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta name="description" content={description ?? description} />
+      <link rel="icon" href="/favicon.ico" />
+      <meta name="theme-color" content="rgb(26, 8, 66)"></meta>
+    </Head>
+  );
 }
