d30e39d4e108f4293ddd1bcd52579e7567765a87
Lucas Consejo <lucas.consejo@ynov.com>
2022-03-19 16:38:31 +0100

    ♻️ refactor: add introduction components
    Add introduction components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Metas from "components/metas";
 import Layout from "components/layout";
 import { ReactElement } from "react";
+import Introduction from "components/Introduction";
 import About from "components/About";
 import Technologies from "components/Technologies";
 
@@ -9,31 +10,7 @@ export default function Home() {
     <div>
       <Metas title="Lucas Consejo | Ingénieur d'étude - Développeur Cdiscount" />
 
-      <div
-        id="intro"
-        className="flex flex-col justify-start items-center sm:flex-row sm:justify-around w-full pt-36 sm:pt-0"
-      >
-        <div className="space-y-4 order-last sm:order-first mt-5 sm:mt-0">
-          <div className="text-white space-y-2">
-            <h1 className="font-bold text-3xl sm:text-5xl text-center sm:text-left">
-              Lucas Consejo
-            </h1>
-            <div className="flex flex-col items-center sm:flex-row font-medium text-xl sm:text-2xl space-x-1 whitespace-nowrap">
-              <h2 className="text-left">Ingénieur d&#39;étude - Développeur</h2>
-              <a
-                href="https://www.cdiscount.com/"
-                target="_blank"
-                rel="noreferrer"
-                className="block typewriter text-orange-cds hover:underline"
-              >
-                @cdiscount.
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="relative w-36 h-36 sm:w-64 sm:h-64"></div>
-      </div>
-
+      <Introduction />
       <About />
       <Technologies />
     </div>
