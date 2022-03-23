9540d5c60bec2be21c1ea218821ef2889106c3b1
Lucas Consejo <lucas.consejo@ynov.com>
2022-03-23 16:26:25 +0100

    💄 style: update footer ui
    Update footer ui

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -53,7 +53,7 @@ const navigation = [
 
 export default function Footer() {
   return (
-    <footer className="">
+    <footer className="mb-5 lg:mb-10 xl:mb-0">
       <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
         <div className="flex justify-center space-x-6 md:order-2">
           {navigation.map((item) => (
@@ -62,7 +62,7 @@ export default function Footer() {
               href={item.href}
               target="_blank"
               rel="noreferrer"
-              className="text-gray-400 hover:text-gray-500"
+              className="text-gray-400 hover:text-white transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-200"
             >
               <span className="sr-only">{item.name}</span>
               <item.icon className="h-7 w-7" aria-hidden="true" />
