6d5e925f3a7b8d42479fc35aaf743a781dcbe89a
Lucas Consejo <lucas.consejo@ynov.com>
2022-05-21 15:27:57 +0200

    🔥 delete: remove unused import

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,6 +1,5 @@
 import dynamic from "next/dynamic";
-import React, { useEffect, useRef, useState } from "react";
-import useLazyLoading from "hooks/useLazyLoading";
+import React from "react";
 import { Header, TopScrollBtn } from "components";
 const Footer = dynamic(() => import("components/Footer"));
 import I18nProvider from "contexts/i18nContext";
