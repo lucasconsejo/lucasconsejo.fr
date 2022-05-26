3a679f3c592e59ae75085566432c6a596b32446d
Lucas Consejo <lucas.consejo@ynov.com>
2022-05-26 11:53:40 +0200

    🐛 fix: fix key missing

--- components/Career/List/index.tsx
+++ components/Career/List/index.tsx
@@ -8,6 +8,7 @@ const List: React.FC<ListProps> = ({ content }) => (
     <ol className="relative border-l border-purple-900 mt-12 md:my-0 w-11/12 md:w-7/12 lg:w-5/12">
         {content.map((item, index) => (
             <Item
+                key={index}
                 urlName={item.urlName}
                 url={item.url}
                 content={content[0]} 
